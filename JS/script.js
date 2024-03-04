67cc8acd1b549a0095a960c3cf61150965f34f0d
aagusvelazquez <velazquezagustinamailen@gmail.com>
2024-03-04 18:20:51 -0300

    Cree page de Productos, Contacto, Donde Estamos. Cree paginas de varios libros.

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,10 @@ const contentFooter = document.querySelector(".contentFooter");
 const login = document.querySelector(".login");
 const create = document.querySelector(".create");
 const container = document.querySelector(".contentLogin");
+const decrementBtn = document.getElementById("decrement");
+const incrementBtn = document.getElementById("increment");
+const countDisplay = document.getElementById("count");
+let count = 0;
 
 loginBtn.addEventListener("click", () => {
     loginFrame.style.display = "block";
@@ -30,3 +34,25 @@ login.onclick = function () {
 create.onclick = function () {
     container.classList.remove("signinForm");
 };
+
+// Función para actualizar el contador y mostrarlo
+function updateCount() {
+  countDisplay.textContent = count;
+}
+
+// Al hacer clic en el botón de incremento, incrementar el contador
+incrementBtn.addEventListener('click', () => {
+  count++;
+  updateCount();
+});
+
+// Al hacer clic en el botón de decremento, decrementar el contador
+decrementBtn.addEventListener('click', () => {
+  if (count > 0) {
+    count--;
+    updateCount();
+  }
+});
+
+// Inicializar el contador al cargar la página
+updateCount();
